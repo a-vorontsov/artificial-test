31b704f078e6d711628c7aef6889dfa28c387f51
Aleksandr Vorontsov <aleksandr@vorontsov.co.uk>
2020-03-11 10:58:58 +0000

    Update json saving, display a saved item in mongo colletion

--- util/json_storage.js
+++ util/json_storage.js
@@ -1,8 +1,9 @@
 "use strict";
 const fs = require("fs");
+const format = require("date-fns/format");
 
 const writeToStorage = (storiesData) => {
-    fs.writeFile(`hacker_news_stories_${Date.now()}.json`, JSON.stringify(storiesData), (err) => {
+    fs.writeFile(`${format(Date.now(), "yy_MM_dd")}_hacker_news_stories.json`, JSON.stringify(storiesData, null, 2), (err) => {
         if (err) console.log(err);
     });
 }
--- util/mongodb_storage.js
+++ util/mongodb_storage.js
@@ -10,21 +10,38 @@ const insertStories = (stories, db, cb) => {
     collection.insertMany(
         stories,
         (err, result) => {
-            if (err) throw err;
+            if (err) console.log("There has been an error with writing to the database. Please ensure that mongodb is running.");
             cb(result);
         }
     );
 }
 
+const readStories = (db, cb) => {
+    const collection = db.collection(MONGO_COLLECTION);
+    collection.findOne({}, (err, result) => {
+        if (err) console.log("There has been an error with reading from the database. Please ensure that mongodb is running.");
+        console.log(result);
+        cb(result);
+    })
+}
+
 const writeToStorage = (storiesData) => {
     MongoClient.connect(MONGO_URL, (err, client) => {
-        if (err) throw err;
+        if (err) console.log("There has been an error with connecting to the database. Please ensure that mongodb is running");
 
         const db = client.db(MONGO_DB);
 
         insertStories(storiesData, db, () => {
             client.close();
-        })
+        });
+    });
+    MongoClient.connect(MONGO_URL, (err, client) => {
+        if (err) console.log("There has been an error with connecting to the database. Please ensure that mongodb is running");
+
+        const db = client.db(MONGO_DB);
+        readStories(db, () => {
+            client.close();
+        });
     });
 }
 
